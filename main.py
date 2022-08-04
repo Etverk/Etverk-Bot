9e9d4e5001a79dd659c5993f4052635b5adf6ae9
Henrik Mårtensson Etverk <54513147+Etverk@users.noreply.github.com>
2022-08-05 01:23:54 +0200

    Update main.py
    Removed redundant empty-lines.

--- main.py
+++ main.py
@@ -9,19 +9,11 @@ bot = commands.Bot(command_prefix="&", intents = discord.Intents.default())
 with open("database.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
     botToken = lines[0]
-
-
-
-
-
-
+    
 #Login confirmation message
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
-
-
-
-
+    
 #Runs bot 
 bot.run(botToken)
